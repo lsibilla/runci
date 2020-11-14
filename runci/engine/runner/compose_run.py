a569050470e3f1af9eec100780d4a60f8eb46fbb
Laurent Sibilla <laurent@sibilla.be>
2020-11-14 17:48:10 +0100

    Add project name setting for compose_run runner

--- runci/engine/runner/compose_run.py
+++ runci/engine/runner/compose_run.py
@@ -7,11 +7,15 @@ class ComposeRunRunner(RunnerBase):
     async def run_internal(self, project: Project):
         files = self.spec.get('file', project.parameters.dataconnection).split(' ')
         service_list = self.spec.get('services', None)
+        project_name = self.spec.get('projectName', None)
 
         args = ['docker-compose']
         for file in files:
             args.extend(['-f', file])
         args.extend(['run', '--rm'])
+
+        if project_name is not None:
+            args.extend(['-p', project_name])
 
         if service_list is not None:
             args.extend(service_list.split(' '))
